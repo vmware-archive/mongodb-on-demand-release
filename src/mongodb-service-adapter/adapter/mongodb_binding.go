c34cb7713630bcce52ab6231f4bea30f2056e8ca
miyer-pivotal <imallika@gmail.com>
2016-08-24 18:52:12 -0400

    changed config agent

--- src/mongodb-service-adapter/adapter/mongodb_binding.go
+++ src/mongodb-service-adapter/adapter/mongodb_binding.go
@@ -35,13 +35,6 @@ func (Binder) CreateBinding(bindingID string, deploymentTopology bosh.BoshVMs, m
 		FailFast:  true,
 	}
 
-  fmt.Printf(" **** Dial info *****")
-	fmt.Printf("%s",dialInfo)
-	fmt.Printf(" **** Dial info End *****")
-
-	fmt.Printf(" **** Servers *****")
-	fmt.Printf("%s",servers)
-	fmt.Printf(" **** Servers End *****")
 	session, err := mgo.DialWithInfo(dialInfo)
 	if err != nil {
 		panic(err)
@@ -68,9 +61,6 @@ func (Binder) CreateBinding(bindingID string, deploymentTopology bosh.BoshVMs, m
 		},
 	}
 	adminDB.UpsertUser(user)
-
-
-	fmt.Printf("This is the connection string :     mongodb://%s:%s@%s/%s?authSource=admin", username, password, strings.Join(servers, ","), username)
 
 	return serviceadapter.Binding{
 		Credentials: map[string]interface{}{
@@ -104,10 +94,6 @@ func (Binder) DeleteBinding(bindingID string, deploymentTopology bosh.BoshVMs, m
 		Database:  "admin",
 		FailFast:  true,
 	}
-
-	fmt.Printf("%s",dialInfo)
-
-	fmt.Printf("%s",servers)
 
 	session, err := mgo.DialWithInfo(dialInfo)
 	if err != nil {
