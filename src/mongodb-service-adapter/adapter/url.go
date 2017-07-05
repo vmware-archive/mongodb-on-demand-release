d7e9ef1bb27d66c3a415f47ba3d6233e3e338271
Alexander Menzhinsky <amenzhinsky@gmail.com>
2017-07-05 14:39:29 +0300

    Implement dashboard url generation

--- src/mongodb-service-adapter/adapter/url.go
+++ src/mongodb-service-adapter/adapter/url.go
@@ -1,13 +1,20 @@
 package adapter
 
 import (
+	"fmt"
+
 	"github.com/pivotal-cf/on-demand-services-sdk/bosh"
 	"github.com/pivotal-cf/on-demand-services-sdk/serviceadapter"
 )
 
-type DashboardURLGenerator struct {}
+type DashboardURLGenerator struct{}
 
 func (d *DashboardURLGenerator) DashboardUrl(instanceID string, plan serviceadapter.Plan, manifest bosh.BoshManifest) (serviceadapter.DashboardUrl, error) {
-	// TODO: implement url generator
-	return serviceadapter.DashboardUrl{DashboardUrl: "http://todo.com"}, nil
+	properties := manifest.Properties["mongo_ops"].(map[interface{}]interface{})
+	url := properties["url"].(string)
+	groupID := properties["group_id"].(string)
+
+	return serviceadapter.DashboardUrl{
+		DashboardUrl: fmt.Sprintf("%s/v2/%s", url, groupID),
+	}, nil
 }
